c258e19ceb08dd0ea4144ccb870e717e91e8c84e
Usuario <Usuario@LAPTOP-P086C5J6>
2020-05-28 17:05:50 -0300

    Asunto: Inicializacion del proyecto Autor: Torrejon Cristian Task00: Inicializacion del Proyecto del Tp5

---
+++ src/test/java/ar/edu/unju/fi/Torrejon3305Tp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Torrejon3305Tp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
